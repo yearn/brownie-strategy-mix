9ccdbcf9003ace9451e6efaf1a8a45e7e602836d
Ser Doggo <3859395+fubuloubu@users.noreply.github.com>
2020-10-27 13:51:56 -0400

    feat: add deployment script for strategy

---
+++ scripts/deploy.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+
+from brownie import Strategy, accounts, config, network, project, web3
+from eth_utils import is_checksum_address
+
+
+API_VERSION = config["dependencies"][0].split("@")[-1]
+Vault = project.load(
+    Path.home() / ".brownie" / "packages" / config["dependencies"][0]
+).Vault
+
+
+def get_address(msg: str) -> str:
+    while True:
+        val = input(msg)
+        if is_checksum_address(val):
+            return val
+        else:
+            addr = web3.ens.address(val)
+            if addr:
+                print(f"Found ENS '{val}' [{addr}]")
+                return addr
+        print(f"I'm sorry, but '{val}' is not a checksummed address or ENS")
+
+
+def main():
+    print(f"You are using the '{network.show_active()}' network")
+    dev = accounts.load("dev")
+    print(f"You are using: 'dev' [{dev.address}]")
+
+    if input("Is there a Vault for this strategy already? y/[N]: ").lower() != "y":
+        vault = Vault.at(get_address("Deployed Vault: "))
+        assert vault.apiVersion() == API_VERSION
+    else:
+        return  # TODO: Deploy one using scripts from Vault project
+
+    print(
+        f"""
+    Strategy Parameters
+
+       api: {API_VERSION}
+     token: {vault.token()}
+      name: '{vault.name()}'
+    symbol: '{vault.symbol()}'
+    """
+    )
+    if input("Deploy Strategy? y/[N]: ").lower() != "y":
+        return
+
+    strategy = Strategy.deploy(vault, {"from": dev})
