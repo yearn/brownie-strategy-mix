02d86c4f01b5896e3f4969230642ccb26102ae7c
Just some guy <3859395+fubuloubu@users.noreply.github.com>
2021-01-13 20:03:01 -0500

    chore: upgrade to vaults v0.3.0 (#13)
    * chore: ignore node_modules/
    * refactor: change alias from `@openzeppelinV3` to `@openzeppelin`
    * refactor: upgrade yearn-vaults to v0.3.0
    * refactor: minReportDelay -> maxReportDelay
    * refactor: name() mutability `pure` -> `view`
    * refactor: remove duplicate documentation (since it's covered)
    * refactor: remove `exitPosition` (removed in v0.3.0)
    * refactor: `liquidatePosition` signature change
    
    NOTE: Also added sample implementation

--- contracts/Strategy.sol
+++ contracts/Strategy.sol
@@ -10,10 +10,10 @@ import {
     BaseStrategy,
     StrategyParams
 } from "@yearnvaults/contracts/BaseStrategy.sol";
-import "@openzeppelinV3/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelinV3/contracts/math/SafeMath.sol";
-import "@openzeppelinV3/contracts/utils/Address.sol";
-import "@openzeppelinV3/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 // Import interfaces for many popular DeFi projects, or add your own!
 //import "../interfaces/<protocol>/<Interface>.sol";
@@ -25,55 +25,23 @@ contract Strategy is BaseStrategy {
 
     constructor(address _vault) public BaseStrategy(_vault) {
         // You can set these parameters on deployment to whatever you want
-        // minReportDelay = 6300;
+        // maxReportDelay = 6300;
         // profitFactor = 100;
         // debtThreshold = 0;
     }
 
     // ******** OVERRIDE THESE METHODS FROM BASE CONTRACT ************
 
-    function name() external override pure returns (string memory) {
+    function name() external override view returns (string memory) {
         // Add your own name here, suggestion e.g. "StrategyCreamYFI"
         return "Strategy<ProtocolName><TokenType>";
     }
 
-    /*
-     * Provide an accurate estimate for the total amount of assets (principle + return)
-     * that this strategy is currently managing, denominated in terms of `want` tokens.
-     * This total should be "realizable" e.g. the total value that could *actually* be
-     * obtained from this strategy if it were to divest it's entire position based on
-     * current on-chain conditions.
-     *
-     * NOTE: care must be taken in using this function, since it relies on external
-     *       systems, which could be manipulated by the attacker to give an inflated
-     *       (or reduced) value produced by this function, based on current on-chain
-     *       conditions (e.g. this function is possible to influence through flashloan
-     *       attacks, oracle manipulations, or other DeFi attack mechanisms).
-     *
-     * NOTE: It is up to governance to use this function in order to correctly order
-     *       this strategy relative to its peers in order to minimize losses for the
-     *       Vault based on sudden withdrawals. This value should be higher than the
-     *       total debt of the strategy and higher than it's expected value to be "safe".
-     */
     function estimatedTotalAssets() public override view returns (uint256) {
         // TODO: Build a more accurate estimate using the value of all positions in terms of `want`
         return want.balanceOf(address(this));
     }
 
-    /*
-     * Perform any strategy unwinding or other calls necessary to capture the "free return"
-     * this strategy has generated since the last time it's core position(s) were adjusted.
-     * Examples include unwrapping extra rewards. This call is only used during "normal operation"
-     * of a Strategy, and should be optimized to minimize losses as much as possible. This method
-     * returns any realized profits and/or realized losses incurred, and should return the total
-     * amounts of profits/losses/debt payments (in `want` tokens) for the Vault's accounting
-     * (e.g. `want.balanceOf(this) >= _debtPayment + _profit - _loss`).
-     *
-     * NOTE: `_debtPayment` should be less than or equal to `_debtOutstanding`. It is okay for it
-     *       to be less than `_debtOutstanding`, as that should only used as a guide for how much
-     *       is left to pay back. Payments should be made to minimize loss from slippage, debt,
-     *       withdrawal fees, etc.
-     */
     function prepareReturn(uint256 _debtOutstanding)
         internal
         override
@@ -88,55 +56,31 @@ contract Strategy is BaseStrategy {
         // NOTE: Should try to free up at least `_debtOutstanding` of underlying position
     }
 
-    /*
-     * Perform any adjustments to the core position(s) of this strategy given
-     * what change the Vault made in the "investable capital" available to the
-     * strategy. Note that all "free capital" in the strategy after the report
-     * was made is available for reinvestment. Also note that this number could
-     * be 0, and you should handle that scenario accordingly.
-     */
     function adjustPosition(uint256 _debtOutstanding) internal override {
         // TODO: Do something to invest excess `want` tokens (from the Vault) into your positions
         // NOTE: Try to adjust positions so that `_debtOutstanding` can be freed up on *next* harvest (not immediately)
     }
 
-    /*
-     * Make as much capital as possible "free" for the Vault to take. Some
-     * slippage is allowed. The goal is for the strategy to divest as quickly as possible
-     * while not suffering exorbitant losses. This function is used during emergency exit
-     * instead of `prepareReturn()`. This method returns any realized losses incurred, and
-     * should also return the amount of `want` tokens available to repay outstanding debt
-     * to the Vault.
-     */
-    function exitPosition(uint256 _debtOutstanding)
-        internal
-        override
-        returns (uint256 _profit, uint256 _loss, uint256 _debtPayment)
-    {
-        // TODO: Do stuff here to free up as much as possible of all positions back into `want`
-        // TODO: returns any realized profit/losses incurred, and should also return the amount
-        // of `want` tokens available to repay back to the Vault.
-    }
-
-    /*
-     * Liquidate as many assets as possible to `want`, irregardless of slippage,
-     * up to `_amountNeeded`. Any excess should be re-invested here as well.
-     */
     function liquidatePosition(uint256 _amountNeeded)
         internal
         override
-        returns (uint256 _amountFreed)
+        returns (uint256 _liquidatedAmount, uint256 _loss)
     {
         // TODO: Do stuff here to free up to `_amountNeeded` from all positions back into `want`
-        // NOTE: Return `_amountFreed`, which should be `<= _amountNeeded`
+        // NOTE: Maintain invariant `want.balanceOf(this) >= _liquidatedAmount`
+        // NOTE: Maintain invariant `_liquidatedAmount + _loss <= _amountNeeded`
+
+        uint256 totalAssets = want.balanceOf(address(this));
+        if (_amountNeeded > totalAssets) {
+            _liquidatedAmount = totalAssets;
+            _loss = _amountNeeded.sub(totalAssets);
+        } else {
+            _liquidatedAmount = _amountNeeded;
+        }
     }
 
     // NOTE: Can override `tendTrigger` and `harvestTrigger` if necessary
 
-    /*
-     * Do anything necesseary to prepare this strategy for migration, such
-     * as transfering any reserve or LP tokens, CDPs, or other tokens or stores of value.
-     */
     function prepareMigration(address _newStrategy) internal override {
         // TODO: Transfer any non-`want` tokens to the new strategy
         // NOTE: `migrate` will automatically forward all `want` in this strategy to the new one
