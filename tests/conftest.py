7b3a73db04075fe52bdfe5a96d6f975da97de90a
steffel <2143646+steffenix@users.noreply.github.com>
2021-02-02 17:30:51 +0100

    feat: add test operation

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,117 +1,57 @@
 import pytest
 from brownie import config
-
-@pytest.fixture
-def andre(accounts):
-    # Andre, giver of tokens, and maker of yield
-    yield accounts[0]
-
-
-@pytest.fixture
-def token(andre, Token):
-    yield andre.deploy(Token)
+from brownie import Contract
 
 
 @pytest.fixture
 def gov(accounts):
-    # yearn multis... I mean YFI governance. I swear!
+    yield accounts[0]
+
+
+@pytest.fixture
+def rewards(accounts):
     yield accounts[1]
 
 
 @pytest.fixture
-def rewards(gov):
-    yield gov  # TODO: Add rewards contract
-
-
-@pytest.fixture
 def guardian(accounts):
-    # YFI Whale, probably
     yield accounts[2]
 
 
 @pytest.fixture
-def vault(pm, gov, rewards, guardian, token):
-    Vault = pm(config["dependencies"][0]).Vault
-    vault = guardian.deploy(Vault, token, gov, rewards, "", "")
-    yield vault
-
-
-@pytest.fixture
-def strategist(accounts):
-    # You! Our new Strategist!
+def management(accounts):
     yield accounts[3]
 
 
 @pytest.fixture
-def keeper(accounts):
-    # This is our trusty bot!
+def strategist(accounts):
     yield accounts[4]
 
 
 @pytest.fixture
-def strategy(strategist, keeper, vault, Strategy):
-    strategy = strategist.deploy(Strategy, vault)
-    strategy.setKeeper(keeper)
-    yield strategy
-
-
-@pytest.fixture
-def nocoiner(accounts):
-    # Has no tokens (DeFi is a ponzi scheme!)
+def keeper(accounts):
     yield accounts[5]
 
 
 @pytest.fixture
-def pleb(accounts, andre, token, vault):
-    # Small fish in a big pond
-    a = accounts[6]
-    # Has 0.01% of tokens (heard about this new DeFi thing!)
-    bal = token.totalSupply() // 10000
-    token.transfer(a, bal, {"from": andre})
-    # Unlimited Approvals
-    token.approve(vault, 2 ** 256 - 1, {"from": a})
-    # Deposit half their stack
-    vault.deposit(bal // 2, {"from": a})
-    yield a
+def token():
+    token_address = "0x6b175474e89094c44da98b954eedeac495271d0f"  # this should be the address of the ERC-20 used by the strategy/vault
+    yield Contract.from_explorer(token_address)
 
 
 @pytest.fixture
-def chad(accounts, andre, token, vault):
-    # Just here to have fun!
-    a = accounts[7]
-    # Has 0.1% of tokens (somehow makes money trying every new thing)
-    bal = token.totalSupply() // 1000
-    token.transfer(a, bal, {"from": andre})
-    # Unlimited Approvals
-    token.approve(vault, 2 ** 256 - 1, {"from": a})
-    # Deposit half their stack
-    vault.deposit(bal // 2, {"from": a})
-    yield a
+def vault(pm, gov, rewards, guardian, management, token):
+    Vault = pm(config["dependencies"][0]).Vault
+    vault = guardian.deploy(Vault)
+    vault.initialize(token, gov, rewards, "", "", guardian)
+    vault.setDepositLimit(2 ** 256 - 1, {"from": gov})
+    vault.setManagement(management, {"from": gov})
+    yield vault
 
 
 @pytest.fixture
-def greyhat(accounts, andre, token, vault):
-    # Chaotic evil, will eat you alive
-    a = accounts[8]
-    # Has 1% of tokens (earned them the *hard way*)
-    bal = token.totalSupply() // 100
-    token.transfer(a, bal, {"from": andre})
-    # Unlimited Approvals
-    token.approve(vault, 2 ** 256 - 1, {"from": a})
-    # Deposit half their stack
-    vault.deposit(bal // 2, {"from": a})
-    yield a
-
-
-@pytest.fixture
-def whale(accounts, andre, token, vault):
-    # Totally in it for the tech
-    a = accounts[9]
-    # Has 10% of tokens (was in the ICO)
-    bal = token.totalSupply() // 10
-    token.transfer(a, bal, {"from": andre})
-    # Unlimited Approvals
-    token.approve(vault, 2 ** 256 - 1, {"from": a})
-    # Deposit half their stack
-    vault.deposit(bal // 2, {"from": a})
-    yield a
+def strategy(strategist, keeper, vault, Strategy, gov):
+    strategy = strategist.deploy(Strategy, vault)
+    strategy.setKeeper(keeper)
+    vault.addStrategy(strategy, 10_000, 10_000, 10, {"from": gov})
+    yield strategy
--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,8 +1,25 @@
-# TODO: Add tests here that show the normal operation of this strategy
-#       Suggestions to include:
-#           - strategy loading and unloading (via Vault addStrategy/revokeStrategy)
-#           - change in loading (from low to high and high to low)
-#           - strategy operation at different loading levels (anticipated and "extreme")
+from brownie import Contract
 
-def test_operation():
-    pass
+
+def test_operation(accounts, token, vault, strategy, strategist):
+    # First you need to get some funds for the token you are about to use,
+    # in this example it impersonate an exchange address to use it's funds.
+    amount = 10_000 * 10e18
+    reserve = accounts.at("0xd551234ae421e3bcba99a0da6d736074f22192ff", force=True)
+    token.transfer(accounts[0], amount, {"from": reserve})
+
+    # Deposit to the vault
+    token.approve(vault.address, amount, {"from": accounts[0]})
+    vault.deposit(amount, {"from": accounts[0]})
+    assert token.balanceOf(vault.address) == amount
+
+    # harvest
+    strategy.harvest()
+    assert token.balanceOf(strategy.address) != 0
+
+    # tend()
+    strategy.tend()
+
+    # withdrawal
+    vault.withdraw({"from": accounts[0]})
+    assert token.balanceOf(accounts[0]) != 0
