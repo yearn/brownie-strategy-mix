b8db8f2cff962b38fed739130c330ad9a5597d85
Steffel <2143646+steffenix@users.noreply.github.com>
2021-02-05 18:18:38 +0100

    feat: add test operation (#16)
    * feat: add test operation
    * fix: suggestions
    * feat: add emergency exit test
    * feat: add github ci

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,8 +1,32 @@
-# TODO: Add tests here that show the normal operation of this strategy
-#       Suggestions to include:
-#           - strategy loading and unloading (via Vault addStrategy/revokeStrategy)
-#           - change in loading (from low to high and high to low)
-#           - strategy operation at different loading levels (anticipated and "extreme")
+from brownie import Contract
 
-def test_operation():
-    pass
+
+def test_operation(accounts, token, vault, strategy, strategist, amount):
+    # Deposit to the vault
+    token.approve(vault.address, amount, {"from": accounts[0]})
+    vault.deposit(amount, {"from": accounts[0]})
+    assert token.balanceOf(vault.address) == amount
+
+    # harvest
+    strategy.harvest()
+    assert token.balanceOf(strategy.address) == amount
+
+    # tend()
+    strategy.tend()
+
+    # withdrawal
+    vault.withdraw({"from": accounts[0]})
+    assert token.balanceOf(accounts[0]) != 0
+
+
+def test_emergency_exit(accounts, token, vault, strategy, strategist, amount):
+    # Deposit to the vault
+    token.approve(vault.address, amount, {"from": accounts[0]})
+    vault.deposit(amount, {"from": accounts[0]})
+    strategy.harvest()
+    assert token.balanceOf(strategy.address) == amount
+
+    # set emergency and exit
+    strategy.setEmergencyExit()
+    strategy.harvest()
+    assert token.balanceOf(strategy.address) < amount
