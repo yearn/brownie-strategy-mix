7254da7b4e199f1c9c12f8e4328eef9535558197
Steffel <2143646+steffenix@users.noreply.github.com>
2021-02-08 18:14:45 +0100

    refactor: update deployment script (#20)
    * feat: add etherscan auto contract auto verification

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from brownie import Strategy, accounts, config, network, project, web3
 from eth_utils import is_checksum_address
-
+import click
 
 API_VERSION = config["dependencies"][0].split("@")[-1]
 Vault = project.load(
@@ -10,28 +10,35 @@ Vault = project.load(
 ).Vault
 
 
-def get_address(msg: str) -> str:
+def get_address(msg: str, default: str = None) -> str:
+    val = click.prompt(msg, default=default)
+
+    # Keep asking user for click.prompt until it passes
     while True:
-        val = input(msg)
+
         if is_checksum_address(val):
             return val
-        else:
-            addr = web3.ens.address(val)
-            if addr:
-                print(f"Found ENS '{val}' [{addr}]")
-                return addr
-        print(f"I'm sorry, but '{val}' is not a checksummed address or ENS")
+        elif addr := web3.ens.address(val):
+            click.echo(f"Found ENS '{val}' [{addr}]")
+            return addr
+
+        click.echo(
+            f"I'm sorry, but '{val}' is not a checksummed address or valid ENS record"
+        )
+        # NOTE: Only display default once
+        val = click.prompt(msg)
 
 
 def main():
     print(f"You are using the '{network.show_active()}' network")
-    dev = accounts.load("dev")
+    dev = accounts.load(click.prompt("Account", type=click.Choice(accounts.load())))
     print(f"You are using: 'dev' [{dev.address}]")
 
-    if input("Is there a Vault for this strategy already? y/[N]: ").lower() != "y":
+    if input("Is there a Vault for this strategy already? y/[N]: ").lower() == "y":
         vault = Vault.at(get_address("Deployed Vault: "))
         assert vault.apiVersion() == API_VERSION
     else:
+        print("You should deploy one vault using scripts from Vault project")
         return  # TODO: Deploy one using scripts from Vault project
 
     print(
@@ -44,7 +51,8 @@ def main():
     symbol: '{vault.symbol()}'
     """
     )
+    publish_source = click.confirm("Verify source on etherscan?")
     if input("Deploy Strategy? y/[N]: ").lower() != "y":
         return
 
-    strategy = Strategy.deploy(vault, {"from": dev})
+    strategy = Strategy.deploy(vault, {"from": dev}, publish_source=publish_source)
