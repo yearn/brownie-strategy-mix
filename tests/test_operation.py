2bad122619f04427ca16168e53fc43b345128897
steffel <2143646+steffenix@users.noreply.github.com>
2021-02-11 09:39:39 +0100

    test: add trigger tests

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -91,3 +91,14 @@ def test_sweep(gov, vault, strategy, token, amount, weth, weth_amout):
     assert weth.balanceOf(gov) == 0
     strategy.sweep(weth, {"from": gov})
     assert weth.balanceOf(gov) == weth_amout
+
+
+def test_triggers(gov, vault, strategy, token, amount, weth, weth_amout):
+    # Deposit to the vault and harvest
+    token.approve(vault.address, amount, {"from": gov})
+    vault.deposit(amount, {"from": gov})
+    vault.updateStrategyDebtRatio(strategy.address, 5_000, {"from": gov})
+    strategy.harvest()
+
+    strategy.harvestTrigger(0)
+    strategy.tendTrigger(0)
