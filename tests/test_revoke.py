a47faa9fa238199dde3e1074ae1538122aa83063
Steffel <2143646+steffenix@users.noreply.github.com>
2021-05-18 18:15:13 +0200

    Update to 0.4 (#45)
    * feat: add ethToWant
    
    * chore: update to vault 0.4.0

--- tests/test_revoke.py
+++ tests/test_revoke.py
@@ -2,30 +2,34 @@ import pytest
 
 
 def test_revoke_strategy_from_vault(
-    token, vault, strategy, amount, user, gov, RELATIVE_APPROX
+    chain, token, vault, strategy, amount, user, gov, RELATIVE_APPROX
 ):
     # Deposit to the vault and harvest
     token.approve(vault.address, amount, {"from": user})
     vault.deposit(amount, {"from": user})
+    chain.sleep(1)
     strategy.harvest()
     assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == amount
 
     # In order to pass this tests, you will need to implement prepareReturn.
     # TODO: uncomment the following lines.
     # vault.revokeStrategy(strategy.address, {"from": gov})
+    # chain.sleep(1)
     # strategy.harvest()
     # assert pytest.approx(token.balanceOf(vault.address), rel=RELATIVE_APPROX) == amount
 
 
 def test_revoke_strategy_from_strategy(
-    token, vault, strategy, amount, gov, user, RELATIVE_APPROX
+    chain, token, vault, strategy, amount, gov, user, RELATIVE_APPROX
 ):
     # Deposit to the vault and harvest
     token.approve(vault.address, amount, {"from": user})
     vault.deposit(amount, {"from": user})
+    chain.sleep(1)
     strategy.harvest()
     assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == amount
 
     strategy.setEmergencyExit()
+    chain.sleep(1)
     strategy.harvest()
     assert pytest.approx(token.balanceOf(vault.address), rel=RELATIVE_APPROX) == amount
