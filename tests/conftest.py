4dabcf305d54b70cc217a7bd8b55f44ba02447f0
steffel <2143646+steffenix@users.noreply.github.com>
2021-06-15 16:56:52 +0200

    v0.3.5

--- tests/conftest.py
+++ tests/conftest.py
@@ -71,7 +71,7 @@ def weth_amout(user, weth):
 def vault(pm, gov, rewards, guardian, management, token):
     Vault = pm(config["dependencies"][0]).Vault
     vault = guardian.deploy(Vault)
-    vault.initialize(token, gov, rewards, "", "", guardian, management)
+    vault.initialize(token, gov, rewards, "", "", guardian)
     vault.setDepositLimit(2 ** 256 - 1, {"from": gov})
     vault.setManagement(management, {"from": gov})
     yield vault
