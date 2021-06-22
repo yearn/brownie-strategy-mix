1bacb376b8e8f8bda579711f747beb231801e98f
jmonteer <jmonteer@protonmail.com>
2021-06-22 17:51:46 +0200

    fix: add file

---
+++ tests/test_manual_operation.py
@@ -0,0 +1,19 @@
+from utils import actions
+
+# TODO: check that all manual operation works as expected
+# manual operation: those functions that are called by management to affect strategy's position
+# e.g. repay debt manually
+# e.g. emergency unstake
+def test_manual_function1(
+    chain, token, vault, strategy, amount, gov, user, RELATIVE_APPROX
+):
+    # set up steady state
+    actions.first_deposit_and_harvest(
+        vault, strategy, token, user, gov, amount, RELATIVE_APPROX
+    )
+    # use manual function
+
+    # shut down strategy and check accounting
+    strategy.updateStrategyDebtRatio(strategy, 0, {"from": gov})
+    strategy.harvest({"from": gov})
+    return
