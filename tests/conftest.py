b172fec43ebf94148f8d957339799dcb892e23d7
jmonteer <jmonteer@protonmail.com>
2021-07-08 16:15:31 +0200

    feat: fn_isolation

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,11 @@ import pytest
 from brownie import config
 from brownie import Contract
 
+# Function scoped isolation fixture to enable xdist.
+# Snapshots the chain before each test and reverts after test completion.
+@pytest.fixture(scope="function", autouse=True)
+def shared_setup(fn_isolation):
+    pass
 
 @pytest.fixture
 def gov(accounts):
