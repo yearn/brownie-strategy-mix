ff33e365d719e789f1ffa63d782c32c37cc39adb
Entreprenerd <valeranijr@gmail.com>
2021-07-21 15:03:43 +0200

    fix: leave TODO message

--- tests/test_shutdown.py
+++ tests/test_shutdown.py
@@ -1,3 +1,6 @@
+# TODO: Add tests that show proper operation of this strategy through "emergencyExit"
+#       Make sure to demonstrate the "worst case losses" as well as the time it takes
+
 import brownie
 import pytest
 
