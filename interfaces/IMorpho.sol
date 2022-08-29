b3f8e9ccd026f230105fe3aa2c2c14a8fe5054fa
Eugen Druzin <edruzin@gmail.com>
2022-08-29 23:29:01 +0200

    style: remove unused functions

--- interfaces/IMorpho.sol
+++ interfaces/IMorpho.sol
@@ -6,38 +6,11 @@ interface IMorpho {
     function supply(
         address _poolTokenAddress,
         address _onBehalf,
-        uint256 _amount
-    ) external;
-
-    function supply(
-        address _poolTokenAddress,
-        address _onBehalf,
-        uint256 _amount,
-        uint256 _maxGasForMatching
-    ) external;
-
-    function borrow(address _poolTokenAddress, uint256 _amount) external;
-
-    function borrow(
-        address _poolTokenAddress,
         uint256 _amount,
         uint256 _maxGasForMatching
     ) external;
 
     function withdraw(address _poolTokenAddress, uint256 _amount) external;
-
-    function repay(
-        address _poolTokenAddress,
-        address _onBehalf,
-        uint256 _amount
-    ) external;
-
-    function liquidate(
-        address _poolTokenBorrowedAddress,
-        address _poolTokenCollateralAddress,
-        address _borrower,
-        uint256 _amount
-    ) external;
 
     function claimRewards(
         address[] calldata _cTokenAddresses,
